928302a34366e4f5f9d7fa899e0e5cc9a54a4589
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-25 03:22:10 +0600

    printing hello world

---
+++ src/helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        //print use for printing a line
+        System.out.print("my name : ");
+        System.out.print("Rifat Abrar Jowad  ");
+        //println use for print and make a line both *
+        System.out.println(" ");// its for making a line gap
+        System.out.println("my name");
+        System.out.println("Rifat Abrar Jowad");
+    }
+}
